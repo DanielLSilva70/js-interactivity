9ebc0fb2a4865a51b6aac153ab77bba7a88b2895
Daniel Silva <silvaldaniel70@gmail.com>
2022-01-19 15:32:41 -0600

    1 done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log('hello world')
+
+
+function addMovie (event) {
+    event.preventDefault()
+
+    const inputField = document.querySelector('input');
+    const movie = document.createElement('li')
+    const movieTitle = document.createElement('span')
+    movieTitle.textContent = inputField.value;
+    movie.appendChild(movieTitle)
+    const list = document.querySelector('ul')
+    list.appendChild(movie)
+        inputField = "";
+
+
+        //d
+}
+
+document.querySelector('form').addEventListener('submit', addMovie)
