b738aa701a467e36e4792669700a917067426744
Daniel Silva <silvaldaniel70@gmail.com>
2022-01-19 16:24:25 -0600

    4 done

--- index.js
+++ index.js
@@ -7,14 +7,35 @@ function addMovie (event) {
     const inputField = document.querySelector('input');
     const movie = document.createElement('li')
     const movieTitle = document.createElement('span')
+
+    movieTitle.textContent = inputField.value;
+    movieTitle.textContent.addEventListener('click',crossOffMovie)
+
+    movie.appendChild(movieTitle)
+
+
     movieTitle.textContent = inputField.value;
     movie.appendChild(movieTitle)
     const list = document.querySelector('ul')
     list.appendChild(movie)
         inputField = "";
 
+        //delete
+        let deleteBtn = document.createElement('button')
+        deleteBtn.textContent = 'x'
+        deleteBtn.addEventListener('click', deleteMovie)
+        movie.appendChild(deleteBtn)
+        
+        
+    }
+    document.querySelector('form').addEventListener('submit',addMovie)
+    
+    function deleteMovie (event) {
+        event.target.parentNode.remove()
+    }
+    function crossOffMovie (event) {
+        event.target.classList.toggle('.checked')
+    }
 
-        //d
-}
 
-document.querySelector('form').addEventListener('submit', addMovie)
+    
